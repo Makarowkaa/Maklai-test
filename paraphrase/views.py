8236822de2e2fa2d1f1667b356716f3ecae39755
Vladyslav Makarov <krats1904@gmail.com>
2023-04-28 15:50:02 +0300

    Views using APIView implemented, returns JsonRepsonse

--- paraphrase/views.py
+++ paraphrase/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from paraphrase.paraphrase_logic import generate_paraphrases
 
-# Create your views here.
+
+class ParaphraseView(APIView):
+    def get(self, request):
+        tree = request.GET.get('tree')
+        limit = request.GET.get('limit', 20)
+
+        if not tree:
+            return JsonResponse({'error': "The 'tree' parameter is required."}, status=400)
+
+        try:
+            limit = int(limit)
+        except ValueError:
+            return JsonResponse({'error': "The 'limit' parameter must be an integer."}, status=400)
+
+        result = generate_paraphrases(tree, limit)
+
+        return JsonResponse(result, json_dumps_params={'indent': 2, 'ensure_ascii': False}, safe=False)
